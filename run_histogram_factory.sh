65975c805e4d19e4e94289c5ea976867531e3936
Zachary Alden Meadows <zachary.alden.meadows@cern.ch>
2016-09-01 10:00:46 -0400

    updating to data16 lumi

--- run_histogram_factory.sh
+++ run_histogram_factory.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-INPUT_FILEPATH=/home/zmeadows/ana/TopBosonTagBackground/DataMCbackground/plotting/raw/all_inputs_rel20p7_data16_mc15c.txt
-OUTPUT_FILEPATH=raw_histograms.root
+INPUT_FILEPATH=/home/zmeadows/ana/TopBosonTagBackground/DataMCbackground/plotting/raw/inputs/08012016_dijet_ttbar_vjets_pythia_only.txt
+OUTPUT_FILEPATH=/home/zmeadows/ana/TopBosonTagBackground/DataMCbackground/plotting/raw/dijet/08012016_pythia_nominal_only_fitting_input_test.root
 DATA_TRIGGER=HLT_j420_a10r_L1J100
 EVENT_SELECTOR=NO_SELECTION
-LUMINOSITY=3.20905
-PROCESS_SYSTEMATICS=true
+LUMINOSITY=3.49756
+PROCESS_SYSTEMATICS=false
 
 if [ $PROCESS_SYSTEMATICS ]; then
     histogram-factory $INPUT_FILEPATH -o $OUTPUT_FILEPATH -t $DATA_TRIGGER -L $LUMINOSITY -E $EVENT_SELECTOR -S
