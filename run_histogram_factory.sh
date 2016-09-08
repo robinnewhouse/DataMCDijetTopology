acd965e14b1fcb6a1f21eca8583372d24f079d00
Zachary Alden Meadows <zachary.alden.meadows@cern.ch>
2016-09-08 13:26:58 -0400

    keeping filepaths up to date

--- run_histogram_factory.sh
+++ run_histogram_factory.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-INPUT_FILEPATH=/home/zmeadows/ana/TopBosonTagBackground/DataMCbackground/plotting/raw/inputs/08012016_dijet_ttbar_vjets_pythia_only.txt
-OUTPUT_FILEPATH=/home/zmeadows/ana/TopBosonTagBackground/DataMCbackground/plotting/raw/dijet/08012016_pythia_nominal_only_fitting_input_test.root
+INPUT_FILEPATH=/home/zmeadows/ana/TopBosonTagBackground/DataMCbackground/plotting/raw/inputs/08012016_all_inputs.txt
+OUTPUT_FILEPATH=/home/zmeadows/ana/TopBosonTagBackground/DataMCbackground/plotting/raw/dijet/08012016_nominal_only.root
 DATA_TRIGGER=HLT_j420_a10r_L1J100
 EVENT_SELECTOR=NO_SELECTION
 LUMINOSITY=3.49756
 PROCESS_SYSTEMATICS=false
 
-if [ $PROCESS_SYSTEMATICS ]; then
-    histogram-factory $INPUT_FILEPATH -o $OUTPUT_FILEPATH -t $DATA_TRIGGER -L $LUMINOSITY -E $EVENT_SELECTOR -S
-else
-    histogram-factory $INPUT_FILEPATH -o $OUTPUT_FILEPATH -t $DATA_TRIGGER -L $LUMINOSITY -E $EVENT_SELECTOR
-fi
+histogram-factory $INPUT_FILEPATH -o $OUTPUT_FILEPATH -t $DATA_TRIGGER -L $LUMINOSITY -E $EVENT_SELECTOR
+
+#if [ $PROCESS_SYSTEMATICS ]; then
+#    histogram-factory $INPUT_FILEPATH -o $OUTPUT_FILEPATH -t $DATA_TRIGGER -L $LUMINOSITY -E $EVENT_SELECTOR -S
+#else
+#    histogram-factory $INPUT_FILEPATH -o $OUTPUT_FILEPATH -t $DATA_TRIGGER -L $LUMINOSITY -E $EVENT_SELECTOR
+#fi
