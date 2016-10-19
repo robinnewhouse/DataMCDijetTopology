b089fc04bfe243452f14f328ab0f1708dba3a361
Zachary Alden Meadows <zachary.alden.meadows@cern.ch>
2016-10-19 09:35:27 -0400

    fix relative paths

--- plotting/batch_plot.sh
+++ plotting/batch_plot.sh
@@ -6,6 +6,6 @@ OUTPUT_ROOT_DIR=$2/$(date "+%d%m%Y")
 
 echo $OUTPUT_ROOT_DIR
 
-python plotting/control_plots_dijet.py $RAW_FILEPATH $OUTPUT_ROOT_DIR
-python plotting/systematics_breakdown_plots.py $RAW_FILEPATH $OUTPUT_ROOT_DIR
-python plotting/tagger_pt_efficiency_plots.py $RAW_FILEPATH $OUTPUT_ROOT_DIR
+python control_plots_dijet.py $RAW_FILEPATH $OUTPUT_ROOT_DIR
+python systematics_breakdown_plots.py $RAW_FILEPATH $OUTPUT_ROOT_DIR
+python tagger_pt_efficiency_plots.py $RAW_FILEPATH $OUTPUT_ROOT_DIR
