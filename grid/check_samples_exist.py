67b1684acc3955dc4d9e22c1dc3aada78b3585ba
Zachary Alden Meadows <zachary.alden.meadows@cern.ch>
2016-10-19 12:56:21 -0400

    switch to JETM6

--- grid/check_samples_exist.py
+++ grid/check_samples_exist.py
@@ -13,15 +13,12 @@ def check_sample_dict(sample_dict):
                     stdout = subprocess.PIPE)
             if (ret_code != 0):
                 num_samples_failed += 1
-                print "NOT FOUND: ", f
+                print "SAMPLES NOT FOUND: ", f
             num_samples_checked += 1
 
+check_sample_dict(samples_JETM6)
 check_sample_dict(samples_AOD)
 check_sample_dict(samples_EVNT)
-check_sample_dict(samples_JETM8)
 
 print "NUMBER SAMPLES CHECKED: ", num_samples_checked
 print "NUMBER SAMPLES FAILED: ", num_samples_failed
-
-if (num_samples_failed == 0):
-    print "All samples exist."
