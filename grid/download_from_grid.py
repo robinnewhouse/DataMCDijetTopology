3adc596a09d64b822ab64396fb322959063ef1c8
Zachary Alden Meadows <zachary.alden.meadows@cern.ch>
2016-10-28 10:17:19 -0400

    regular update

--- grid/download_from_grid.py
+++ grid/download_from_grid.py
@@ -2,11 +2,8 @@
 import TopExamples.grid
 
 scope      = 'user.zmeadows'
-dir_data16 = '/titan_rw/atlas/common/zmeadows/TopBosonTagging/dijet/09072016/data16'
-dir_mc15c  = '/titan_rw/atlas/common/zmeadows/TopBosonTagging/dijet/09072016/mc15c'
+dir_data16 = '/titan_rw/atlas/common/zmeadows/TopBosonTagging/dijet/10242016/data16'
+dir_mc15c  = '/titan_rw/atlas/common/zmeadows/TopBosonTagging/dijet/10242016/mc15c'
 
-TopExamples.grid.download(scope , '*data16_dijet_0907_v0_output.root' , dir_data16)
-TopExamples.grid.download(scope , '*wz_plus_jets_0907_v0_output.root' , dir_mc15c)
-TopExamples.grid.download(scope , '*ttbar_0907_v0_output.root'        , dir_mc15c)
-TopExamples.grid.download(scope , '*herwig_dijet_0907_v0_output.root' , dir_mc15c)
-TopExamples.grid.download(scope , '*pythia_dijet_0907_v0_output.root' , dir_mc15c)
+TopExamples.grid.download(scope , 'user.zmeadows.*.data16_10242016_v1_output.root' , dir_data16)
+TopExamples.grid.download(scope , 'user.zmeadows.*.mc15c_10242016_v1_output.root' , dir_mc15c)
