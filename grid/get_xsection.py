d57e36b7b33e9d2cd9ea123fc880225c725c5a49
Zachary Alden Meadows <zachary.alden.meadows@cern.ch>
2016-11-07 12:55:57 -0500

    get NEvents from AOD

--- grid/get_xsection.py
+++ grid/get_xsection.py
@@ -2,7 +2,7 @@ import pyAMI.client
 import pyAMI.atlas.api as api
 import json
 import sys
-from samples import samples_EVNT
+from samples import *
 
 my_weights_file = open("sample_weights.txt", 'w')
 analysis_top_weights_file = open("sample_weights_AnalysisTopFormat.txt", 'w')
@@ -13,41 +13,51 @@ api.init()
 # INSTANTIATE THE PYAMI CLIENT FOR ATLAS
 client = pyAMI.client.Client('atlas')
 
-fields = 'files.cross_section,files.gen_filt_eff,nfiles,files.events,dataset_number,generator_name'
-
 my_weights_file.write("# DSID NUM_EVENTS X_SECTION(fb) FILTER_EFFICIENCY GENERATOR\n")
 
-for sample_name, sample_files in samples_EVNT.iteritems():
+for sample_name in samples_AOD.keys():
 
     my_weights_file.write("# " + sample_name + "\n")
     analysis_top_weights_file.write("# " + sample_name + "\n")
 
     print("# " + sample_name)
 
-    for f in sample_files:
-        res_dict = api.get_dataset_info(client, f)
+    evnt_files = samples_EVNT[sample_name]
+    aod_files = samples_AOD[sample_name]
 
-        if len(res_dict) == 0:
-            print "WARNING: could not find file: ", f
+    # each EVNT file must correspond to exactly one AOD file
+    assert(len(evnt_files) == len(aod_files))
 
-        if len(res_dict) > 1:
-            print "WARNING: result dictionary length greater than one. shouldn't happen."
+    for i_file in range(len(evnt_files)):
+        f_aod  = aod_files[i_file]
+        f_evnt = evnt_files[i_file]
+
+        # we have to look at the AOD file just for NEvents
+        # since not all generated events necessarily make it into the AOD
+        aod_res_dict = api.get_dataset_info(client, f_aod)
+        evnt_res_dict = api.get_dataset_info(client, f_evnt)
+
+        if len(aod_res_dict) == 0:
+            print "WARNING: could not find file: ", f_aod
+            sys.exit(1)
+        if len(evnt_res_dict) == 0:
+            print "WARNING: could not find file: ", f_evnt
             sys.exit(1)
 
-        total_events = res_dict[0]["totalEvents"]
-        filt_eff_mean = res_dict[0]["GenFiltEff_mean"]
-        xsection_mean = res_dict[0]["crossSection_mean"]
-        dsid = res_dict[0]["datasetNumber"]
-        generator_name = res_dict[0]["generatorName"]
+        total_events   = aod_res_dict[0]["totalEvents"]
+        filt_eff_mean  = evnt_res_dict[0]["GenFiltEff_mean"]
+        xsection_mean  = evnt_res_dict[0]["crossSection_mean"]
+        dsid           = evnt_res_dict[0]["datasetNumber"]
+        generator_name = evnt_res_dict[0]["generatorName"]
 
-        if (res_dict[0]["crossSection_unit"] == "nano barn"):
+        if (evnt_res_dict[0]["crossSection_unit"] == "nano barn"):
             xsection_mean = str(float(xsection_mean) * 1e6)
-        elif (res_dict[0]["crossSection_unit"] == "pico barn"):
+        elif (evnt_res_dict[0]["crossSection_unit"] == "pico barn"):
             xsection_mean = str(float(xsection_mean) * 1e3)
-        elif (res_dict[0]["crossSection_unit"] == "femto barn"):
+        elif (evnt_res_dict[0]["crossSection_unit"] == "femto barn"):
             pass
         else:
-            print "unrecognized cross section unit: ", res_dict[0]["crossSection_unit"]
+            print "unrecognized cross section unit: ", evnt_res_dict[0]["crossSection_unit"]
             sys.exit(1)
 
         my_weights_str = ' '.join([dsid, total_events, xsection_mean, filt_eff_mean, generator_name])
