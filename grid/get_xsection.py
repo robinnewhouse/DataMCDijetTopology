b30680669703d253f62d302abcba979436cfbf94
Zac Meadows <zachary.alden.meadows@cern.ch>
2017-06-10 13:50:58 +0200

    only need to use DxAOD sample names now

--- grid/get_xsection.py
+++ grid/get_xsection.py
@@ -2,7 +2,9 @@ import pyAMI.client
 import pyAMI.atlas.api as api
 import json
 import sys
-from samples import *
+from samples_vvjj import *
+
+these_samples = samples_EXOT3
 
 my_weights_file = open("sample_weights.txt", 'w')
 analysis_top_weights_file = open("sample_weights_AnalysisTopFormat.txt", 'w')
@@ -15,30 +17,31 @@ client = pyAMI.client.Client('atlas')
 
 my_weights_file.write("# DSID NUM_EVENTS X_SECTION(fb) FILTER_EFFICIENCY GENERATOR\n")
 
-for sample_name in samples_AOD.keys():
-    # if ("gamma" not in sample_name and "Photon" not in sample_name):
-    #   continue
-    if ("sherpa_gamma" not in sample_name):
+for sample_name, sample_files in these_samples.iteritems():
+    if ("data" in sample_name):
       continue
+
+    print("# " + sample_name)
     my_weights_file.write("# " + sample_name + "\n")
     analysis_top_weights_file.write("# " + sample_name + "\n")
 
-    print("# " + sample_name)
+    for sample_name in sample_files:
+        dsId = sample_name.split('.')[1]
+        evnt_ds = None
+        aod_ds = None
+        provD=api.get_dataset_prov(client, sample_name)
 
-    evnt_files = samples_EVNT[sample_name]
-    aod_files = samples_AOD[sample_name]
-
-    # each EVNT file must correspond to exactly one AOD file
-    assert(len(evnt_files) == len(aod_files))
-
-    for i_file in range(len(evnt_files)):
-        f_aod  = aod_files[i_file]
-        f_evnt = evnt_files[i_file]
+        for dsDic in provD['node']:
+            #print dsDic['dataType'], dsDic['logicalDatasetName']
+            if 'EVNT' == dsDic['dataType'] and dsId in dsDic['logicalDatasetName']:
+                evnt_ds = dsDic['logicalDatasetName']
+            elif aod_ds == None and 'AOD' == dsDic['dataType'] and dsId in dsDic['logicalDatasetName']:
+                aod_ds = dsDic['logicalDatasetName']
 
         # we have to look at the AOD file just for NEvents
         # since not all generated events necessarily make it into the AOD
-        aod_res_dict = api.get_dataset_info(client, f_aod)
-        evnt_res_dict = api.get_dataset_info(client, f_evnt)
+        aod_res_dict = api.get_dataset_info(client, aod_ds)
+        evnt_res_dict = api.get_dataset_info(client, evnt_ds)
 
         if len(aod_res_dict) == 0:
             print "WARNING: could not find file: ", f_aod
@@ -47,20 +50,45 @@ for sample_name in samples_AOD.keys():
             print "WARNING: could not find file: ", f_evnt
             sys.exit(1)
 
+        print aod_res_dict
         dsid           = evnt_res_dict[0]["datasetNumber"]
-        if (dsid != "361062"): continue
         total_events   = aod_res_dict[0]["totalEvents"]
         filt_eff_mean  = evnt_res_dict[0]["GenFiltEff_mean"]
         xsection_mean  = evnt_res_dict[0]["crossSection_mean"]
         generator_name = evnt_res_dict[0]["generatorName"]
 
-        if "N/A" in filt_eff_mean: continue
+        try:
+          map(float, [dsid, total_events, filt_eff_mean, xsection_mean])
+        except ValueError:
+          tmp_fields = 'files.cross_section,files.gen_filt_eff,nfiles,files.events,dataset_number,generator_name'
+          evnt_files = api.list_datasets(client, patterns = evnt_ds, fields = tmp_fields)
 
-        if (evnt_res_dict[0]["crossSection_unit"] == "nano barn"):
+          dsid           = None
+          total_events   = 0.0
+          filt_eff_mean  = 0.0
+          xsection_mean  = 0.0
+          generator_name = None
+
+          numFiles = 0.0
+
+          for results in evnt_files:
+            numFiles = (float)(results['nfiles'])
+            if (results['generator_name'] != 'Null'): generator_name = results['generator_name']
+            if (results['dataset_number'] != 'Null'): dsid = (int) (results['dataset_number'])
+            if (results['files_gen_filt_eff'] != 'NULL'): filt_eff_mean += (float) (results['files_gen_filt_eff'])
+            if (results['files_cross_section'] != 'NULL'): xsection_mean += (float) (results['files_cross_section'])
+            if (results['files_events'] != 'NULL'): total_events += (float) (results['files_events'])
+
+          dsid           = str(dsid)
+          total_events   = str(int(total_events))
+          filt_eff_mean  = str(filt_eff_mean/numFiles)
+          xsection_mean  = str(xsection_mean/numFiles)
+
+        if ("nano" in evnt_res_dict[0]["crossSection_unit"]):
             xsection_mean = str(float(xsection_mean) * 1e6)
-        elif (evnt_res_dict[0]["crossSection_unit"] == "pico barn"):
+        elif ("pico" in evnt_res_dict[0]["crossSection_unit"]):
             xsection_mean = str(float(xsection_mean) * 1e3)
-        elif (evnt_res_dict[0]["crossSection_unit"] == "femto barn"):
+        elif ("femto" in evnt_res_dict[0]["crossSection_unit"]):
             pass
         else:
             print "unrecognized cross section unit: ", evnt_res_dict[0]["crossSection_unit"]
