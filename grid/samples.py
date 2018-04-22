09b5ab57f0ec84b040be0463fe200c5e3466eb10
Robin Newhouse <robin.newhouse@cern.ch>
2018-04-22 10:45:39 -0700

    Making minor modifications to grid submission scripts for resubmitting and downloading jobs

--- grid/samples.py
+++ grid/samples.py
@@ -5,6 +5,58 @@ samples_AOD = {}
 samples_EVNT = {}
 samples_DEBUG = {} # just for individual set testing and downloading xsection files. Shouldn't be used in production
 
+samples_DXAOD['resubmit_dijet'] = [ # X/pb
+        # "mc15_13TeV.426044.HerwigppEvtGen_UEEE5_CTEQ6L1_jetjet_JZ4W.merge.DAOD_JETM6.e4410_s2608_r7725_r7676_p3297",
+        # "mc15_13TeV.426045.HerwigppEvtGen_UEEE5_CTEQ6L1_jetjet_JZ5W.merge.DAOD_JETM6.e4410_s2608_r7725_r7676_p3297",
+        "mc15_13TeV.426046.HerwigppEvtGen_UEEE5_CTEQ6L1_jetjet_JZ6W.merge.DAOD_JETM6.e4410_s2608_r7725_r7676_p3297",
+        "mc15_13TeV.426047.HerwigppEvtGen_UEEE5_CTEQ6L1_jetjet_JZ7W.merge.DAOD_JETM6.e4410_s2608_r7725_r7676_p3297",
+        "mc15_13TeV.426048.HerwigppEvtGen_UEEE5_CTEQ6L1_jetjet_JZ8W.merge.DAOD_JETM6.e4410_s2608_r7725_r7676_p3297",
+        "mc15_13TeV.426049.HerwigppEvtGen_UEEE5_CTEQ6L1_jetjet_JZ9W.merge.DAOD_JETM6.e4410_s2608_r7725_r7676_p3297",
+        "mc15_13TeV.426050.HerwigppEvtGen_UEEE5_CTEQ6L1_jetjet_JZ10W.merge.DAOD_JETM6.e4410_s2608_r7725_r7676_p3297",
+        "mc15_13TeV.426051.HerwigppEvtGen_UEEE5_CTEQ6L1_jetjet_JZ11W.merge.DAOD_JETM6.e4410_s2608_r7772_r7676_p3297",
+        "mc15_13TeV.426052.HerwigppEvtGen_UEEE5_CTEQ6L1_jetjet_JZ12W.merge.DAOD_JETM6.e4410_s2608_r7772_r7676_p3297",
+
+        #sherpa
+        # "mc15_13TeV.426131.Sherpa_CT10_jets_JZ1.merge.DAOD_JETM6.e4355_s2608_r7725_r7676_p3297",
+        # "mc15_13TeV.426132.Sherpa_CT10_jets_JZ2.merge.DAOD_JETM6.e4355_s2608_r7725_r7676_p3297",
+        # "mc15_13TeV.426133.Sherpa_CT10_jets_JZ3.merge.DAOD_JETM6.e4355_s2608_r7725_r7676_p3297",
+        # "mc15_13TeV.426134.Sherpa_CT10_jets_JZ4.merge.DAOD_JETM6.e4355_s2608_r7725_r7676_p3297",
+        # "mc15_13TeV.426135.Sherpa_CT10_jets_JZ5.merge.DAOD_JETM6.e4355_s2608_r7725_r7676_p3297",
+        "mc15_13TeV.426136.Sherpa_CT10_jets_JZ6.merge.DAOD_JETM6.e4355_s2608_r7725_r7676_p3297",
+        # "mc15_13TeV.426137.Sherpa_CT10_jets_JZ7.merge.DAOD_JETM6.e4635_s2726_r7725_r7676_p3297",
+        "mc15_13TeV.426138.Sherpa_CT10_jets_JZ8.merge.DAOD_JETM6.e4635_s2726_r7725_r7676_p3297",
+        "mc15_13TeV.426139.Sherpa_CT10_jets_JZ9.merge.DAOD_JETM6.e4635_s2726_r7725_r7676_p3297",
+        "mc15_13TeV.426140.Sherpa_CT10_jets_JZ10.merge.DAOD_JETM6.e4635_s2726_r7725_r7676_p3297",
+        "mc15_13TeV.426141.Sherpa_CT10_jets_JZ11.merge.DAOD_JETM6.e4635_s2726_s2183_r7725_r7676_p3297",
+        "mc15_13TeV.426142.Sherpa_CT10_jets_JZ12.merge.DAOD_JETM6.e4635_s2726_s2183_r7725_r7676_p3297",
+
+        #pythia
+        # "mc15_13TeV.361024.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ4W.merge.DAOD_JETM6.e3668_s2576_s2132_r7725_r7676_p3297",
+        # "mc15_13TeV.361025.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ5W.merge.DAOD_JETM6.e3668_s2576_s2132_r7725_r7676_p3297",
+        # "mc15_13TeV.361026.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ6W.merge.DAOD_JETM6.e3569_s2608_s2183_r7725_r7676_p3297",
+        # "mc15_13TeV.361027.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ7W.merge.DAOD_JETM6.e3668_s2608_s2183_r7725_r7676_p3297",
+        "mc15_13TeV.361028.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ8W.merge.DAOD_JETM6.e3569_s2576_s2132_r7772_r7676_p3297",
+        "mc15_13TeV.361029.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ9W.merge.DAOD_JETM6.e3569_s2576_s2132_r7772_r7676_p3297",
+        "mc15_13TeV.361030.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ10W.merge.DAOD_JETM6.e3569_s2576_s2132_r7772_r7676_p3297",
+        "mc15_13TeV.361031.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ11W.merge.DAOD_JETM6.e3569_s2608_s2183_r7772_r7676_p3297",
+        "mc15_13TeV.361032.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ12W.merge.DAOD_JETM6.e3668_s2608_s2183_r7772_r7676_p3297",
+
+        # "mc15_13TeV.303722.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_allhad_mtt_1.merge.DAOD_JETM6.e4321_s2608_r7772_r7676_p3297",
+
+    ]
+
+samples_DXAOD['resubmit_gammajet'] = [ # X/pb
+        # "mc15_13TeV.423104.Pythia8EvtGen_A14NNPDF23LO_gammajet_DP140_280.merge.DAOD_JETM6.e3791_s2608_s2183_r7725_r7676_p3297",
+        # "mc15_13TeV.423106.Pythia8EvtGen_A14NNPDF23LO_gammajet_DP500_800.merge.DAOD_JETM6.e3791_s2608_s2183_r7725_r7676_p3297",
+        # "mc15_13TeV.423112.Pythia8EvtGen_A14NNPDF23LO_gammajet_DP3000_inf.merge.DAOD_JETM6.e4453_s2726_r7772_r7676_p3297",
+        # "mc15_13TeV.305440.Sherpa_CT10_ZqqGammaPt140_280.merge.DAOD_JETM6.e5020_s2726_r7772_r7676_p3297",
+        "mc15_13TeV.361048.Sherpa_CT10_SinglePhotonPt280_500_CVetoBVeto.merge.DAOD_JETM6.e3587_s2608_s2183_r7725_r7676_p3297",
+
+
+    ]
+
+
+
 # {{{ DATA
 
 samples_DXAOD['data_2016'] = [ # X/pb
@@ -34,22 +86,6 @@ samples_DXAOD['data_2015'] = [ # 3212.96/pb
 # }}}
 
 # {{{ DIJET
-
-# samples_DXAOD['pythia_dijet_mc15c'] = [
-#     "mc15_13TeV.361020.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ0W.merge.DAOD_JETM6.e3569_s2576_s2132_r7725_r7676_p3297",
-#     "mc15_13TeV.361021.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ1W.merge.DAOD_JETM6.e3569_s2576_s2132_r7725_r7676_p3297",
-#     "mc15_13TeV.361022.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ2W.merge.DAOD_JETM6.e3668_s2576_s2132_r7725_r7676_p3297",
-#     "mc15_13TeV.361023.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ3W.merge.DAOD_JETM6.e3668_s2576_s2132_r7725_r7676_p3297",
-#     "mc15_13TeV.361024.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ4W.merge.DAOD_JETM6.e3668_s2576_s2132_r7725_r7676_p3297",
-#     "mc15_13TeV.361025.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ5W.merge.DAOD_JETM6.e3668_s2576_s2132_r7725_r7676_p3297",
-#     "mc15_13TeV.361026.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ6W.merge.DAOD_JETM6.e3569_s2608_s2183_r7725_r7676_p3297",
-#     "mc15_13TeV.361027.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ7W.merge.DAOD_JETM6.e3668_s2608_s2183_r7725_r7676_p3297",
-#     "mc15_13TeV.361028.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ8W.merge.DAOD_JETM6.e3569_s2576_s2132_r7772_r7676_p3297",
-#     "mc15_13TeV.361029.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ9W.merge.DAOD_JETM6.e3569_s2576_s2132_r7772_r7676_p3297",
-#     "mc15_13TeV.361030.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ10W.merge.DAOD_JETM6.e3569_s2576_s2132_r7772_r7676_p3297",
-#     "mc15_13TeV.361031.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ11W.merge.DAOD_JETM6.e3569_s2608_s2183_r7772_r7676_p3297",
-#     "mc15_13TeV.361032.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ12W.merge.DAOD_JETM6.e3668_s2608_s2183_r7772_r7676_p3297"
-#     ]
 
 samples_DXAOD['pythia_dijet'] = [
     "mc15_13TeV.361020.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ0W.merge.DAOD_JETM6.e3569_s2576_s2132_r7725_r7676_p3297",
@@ -129,18 +165,6 @@ samples_DXAOD["sherpa_zjets"] = [
         "mc15_13TeV.304709.Sherpa_CT10_Zqq_Pt1000.merge.DAOD_JETM6.e4692_s2726_r7772_r7676_p3297",
         ]
 
-# samples_DXAOD["V+jets_mc15c"] = [
-#         #"mc15_13TeV.304673.Herwigpp_UEEE5CTEQ6L1_Wjhadronic_280_500.merge.DAOD_JETM6.e4571_s2726_r7772_r7676_p3297",
-#         #"mc15_13TeV.304674.Herwigpp_UEEE5CTEQ6L1_Wjhadronic_500_700.merge.DAOD_JETM6.e4571_s2726_r7772_r7676_p3297",
-#         #"mc15_13TeV.304675.Herwigpp_UEEE5CTEQ6L1_Wjhadronic_700_1000.merge.DAOD_JETM6.e4571_s2726_r7772_r7676_p3297",
-#         #"mc15_13TeV.304676.Herwigpp_UEEE5CTEQ6L1_Wjhadronic_1000_1400.merge.DAOD_JETM6.e4571_s2726_r7772_r7676_p3297",
-#         #"mc15_13TeV.304677.Herwigpp_UEEE5CTEQ6L1_Wjhadronic_1400.merge.DAOD_JETM6.e4571_s2726_r7772_r7676_p3297",
-#         #"mc15_13TeV.304678.Herwigpp_UEEE5CTEQ6L1_Zjhadronic_280_500.merge.DAOD_JETM6.e4571_s2726_r7772_r7676_p3297",
-#         #"mc15_13TeV.304679.Herwigpp_UEEE5CTEQ6L1_Zjhadronic_500_700.merge.DAOD_JETM6.e4571_s2726_r7772_r7676_p3297",
-#         #"mc15_13TeV.304680.Herwigpp_UEEE5CTEQ6L1_Zjhadronic_700_1000.merge.DAOD_JETM6.e4571_s2726_r7772_r7676_p3297",
-#         #"mc15_13TeV.304681.Herwigpp_UEEE5CTEQ6L1_Zjhadronic_1000_1400.merge.DAOD_JETM6.e4571_s2726_r7772_r7676_p3297",
-#         #"mc15_13TeV.304682.Herwigpp_UEEE5CTEQ6L1_Zjhadronic_1400.merge.DAOD_JETM6.e4571_s2726_r7772_r7676_p3297"
-#         ]
 # }}}
 
 # {{{ allhad ttbar
@@ -191,9 +215,10 @@ samples_DXAOD["sherpa_gammajet"] = [
     "mc15_13TeV.361057.Sherpa_CT10_SinglePhotonPt2000_4000_CVetoBVeto.merge.DAOD_JETM6.e3587_s2608_s2183_r7725_r7676_p3297",
     "mc15_13TeV.361058.Sherpa_CT10_SinglePhotonPt2000_4000_CFilterBVeto.merge.DAOD_JETM6.e3587_s2608_s2183_r7725_r7676_p3297",
     "mc15_13TeV.361059.Sherpa_CT10_SinglePhotonPt2000_4000_BFilter.merge.DAOD_JETM6.e3587_s2608_s2183_r7725_r7676_p3297",
-    "mc15_13TeV.361060.Sherpa_CT10_SinglePhotonPt4000_CVetoBVeto.merge.DAOD_JETM6.e3587_s2608_s2183_r8084_r7676_p3297",
-    "mc15_13TeV.361061.Sherpa_CT10_SinglePhotonPt4000_CFilterBVeto.merge.DAOD_JETM6.e3587_s2608_s2183_r8129_r7676_p3297",
-    "mc15_13TeV.361062.Sherpa_CT10_SinglePhotonPt4000_BFilter.merge.DAOD_JETM6.e3587_s2608_s2183_r8084_r7676_p3297"
+    # temporarily hidden to get xsections (issue with pyAMI)
+    # "mc15_13TeV.361060.Sherpa_CT10_SinglePhotonPt4000_CVetoBVeto.merge.DAOD_JETM6.e3587_s2608_s2183_r8084_r7676_p3297",
+    # "mc15_13TeV.361061.Sherpa_CT10_SinglePhotonPt4000_CFilterBVeto.merge.DAOD_JETM6.e3587_s2608_s2183_r8129_r7676_p3297",
+    # "mc15_13TeV.361062.Sherpa_CT10_SinglePhotonPt4000_BFilter.merge.DAOD_JETM6.e3587_s2608_s2183_r8084_r7676_p3297"
     ]
 
 # }}}
@@ -218,7 +243,8 @@ samples_DXAOD["wz_gamma"] = [
 # {{{ SM ttbar + gamma
 
 samples_DXAOD["ttbar_gamma"] = [
-    "mc15_13TeV.410087.MadGraphPythia8EvtGen_A14NNPDF23LO_ttgamma_allhad.merge.DAOD_JETM6.e4404_s2726_r7772_r7676_p3297"
+    "mc15_13TeV.410087.MadGraphPythia8EvtGen_A14NNPDF23LO_ttgamma_allhad.merge.DAOD_JETM6.e4404_s2726_r7772_r7676_p3395"
+    # "group.perf-jets.410087.MadGraphPythia8EvtGen_ttgamma_allhad_customJETM6_v0_EXT0", # local derivation
     ]
 
 # }}}
@@ -231,20 +257,20 @@ samples_DXAOD["ttbar_gamma"] = [
 
 # NOTE: doesn't check if sample names are valid before writing out.
 if __name__ == '__main__':
-    f_evnt        = open('../data/samples_mc15c_EVNT.txt'   , 'w')
-    f_aod         = open('../data/samples_mc15c_AOD.txt'    , 'w')
+    # f_evnt        = open('../data/samples_mc15c_EVNT.txt'   , 'w')
+    # f_aod         = open('../data/samples_mc15c_AOD.txt'    , 'w')
     f_jetm6_mc15c = open('../data/samples_mc15c_JETM6.txt'  , 'w')
     f_jetm6_data  = open('../data/samples_data16_JETM6.txt' , 'w')
 
-    for key, val in samples_EVNT.iteritems():
-        if "data" not in key:
-            for x in val:
-                f_evnt.write(x + "\n")
+    # for key, val in samples_EVNT.iteritems():
+    #     if "data" not in key:
+    #         for x in val:
+    #             f_evnt.write(x + "\n")
 
-    for key, val in samples_AOD.iteritems():
-        if "data" not in key:
-            for x in val:
-                f_aod.write(x + "\n")
+    # for key, val in samples_AOD.iteritems():
+    #     if "data" not in key:
+    #         for x in val:
+    #             f_aod.write(x + "\n")
 
     for key, val in samples_DXAOD.iteritems():
         if "data" not in key:
@@ -254,8 +280,8 @@ if __name__ == '__main__':
             for x in val:
                 f_jetm6_data.write(x + "\n")
 
-    f_aod.close()
-    f_evnt.close()
+    # f_aod.close()
+    # f_evnt.close()
     f_jetm6_data.close()
     f_jetm6_mc15c.close()
 
--- grid/submit_dijet_all.py
+++ grid/submit_dijet_all.py
@@ -2,7 +2,7 @@
 import TopExamples.grid
 from samples import samples_DXAOD
 
-suffix = 'dijet_20180405_syst'
+suffix = 'dijet_20180405_syst_v4'
 
 mc_config = TopExamples.grid.Config()
 mc_config.settingsFile  = 'cuts/cuts_boosted_dijet_mc15c.txt'
@@ -15,23 +15,27 @@ data_config.suffix         = 'data_'+suffix
 for config in [mc_config, data_config]:
     config.code          = 'top-xaod'
     config.gridUsername  = 'rnewhous'
-    config.excludedSites = 'US,ANALY_BNL_LONG'
-    config.noSubmit      = True
+    config.excludedSites = 'ANALY_MPPMU'
+    config.noSubmit      = False
     config.memory        = '1999' # A lot of sites are short on RAM, so we avoid brokerage issues this way
     config.mergeType     = 'Default' #'None', 'Default' or 'xAOD'
     config.destSE        = '' #This is the default (anywhere), or try e.g. 'UKI-SOUTHGRID-BHAM-HEP_LOCALGROUPDISK'
+    # config.otherOptions  = '--maxNFilesPerJob=5'
 
-
-data_sample_names = [ 'data_2015', 'data_2016' ]
+data_sample_names = [ 
+        # 'data_2015',
+        # 'data_2016' 
+         ]
 mc_sample_names = [
-        'pythia_dijet',
-        'herwig_dijet',
-        'sherpa_dijet',
-        'pythia_wjets',
-        'pythia_zjets',
-        'sherpa_wjets',
-        'sherpa_zjets',
-        'ttbar_allhad'
+        # 'pythia_dijet',
+        # 'herwig_dijet',
+        # 'sherpa_dijet',
+        # 'pythia_wjets',
+        # 'pythia_zjets',
+        # 'sherpa_wjets',
+        # 'sherpa_zjets',
+        # 'ttbar_allhad',
+        'resubmit_dijet',
         ]
 
 for sample_name in mc_sample_names + data_sample_names:
