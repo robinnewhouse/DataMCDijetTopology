ce902d7b87c809079c1bb7129338b6be7226578e
Robin Newhouse <robin.newhouse@cern.ch>
2018-05-04 13:21:28 -0700

    Modified options for grid run, sending now

--- grid/submit_dijet_all.py
+++ grid/submit_dijet_all.py
@@ -2,7 +2,7 @@
 import TopExamples.grid
 from samples import samples_DXAOD
 
-suffix = 'dijet_20180405_syst_v7'
+suffix = 'dijet_20180504_syst'
 
 mc_config = TopExamples.grid.Config()
 mc_config.settingsFile  = 'cuts/cuts_boosted_dijet_mc15c.txt'
@@ -16,30 +16,29 @@ for config in [mc_config, data_config]:
     config.code          = 'top-xaod'
     config.gridUsername  = 'rnewhous'
     config.excludedSites = 'ANALY_MPPMU'
-    config.noSubmit      = False
+    config.noSubmit      = True
     config.memory        = '1999' # A lot of sites are short on RAM, so we avoid brokerage issues this way
     config.mergeType     = 'Default' #'None', 'Default' or 'xAOD'
     config.destSE        = '' #This is the default (anywhere), or try e.g. 'UKI-SOUTHGRID-BHAM-HEP_LOCALGROUPDISK'
-    config.otherOptions  = '--maxNFilesPerJob=1'
+mc_config.otherOptions  = '--maxNFilesPerJob=1'
 
 data_sample_names = [ 
         'data_2015',
         'data_2016' 
          ]
 mc_sample_names = [
-        # 'pythia_dijet',
-        # 'herwig_dijet',
-        # 'sherpa_dijet',
-        # 'pythia_wjets',
-        # 'pythia_zjets',
-        # 'sherpa_wjets',
-        # 'sherpa_zjets',
-        # 'ttbar_allhad',
-        'resubmit_dijet',
+        'pythia_dijet',
+        'herwig_dijet',
+        'sherpa_dijet',
+        'pythia_wjets',
+        'pythia_zjets',
+        'sherpa_wjets',
+        'sherpa_zjets',
+        'ttbar_allhad',
         ]
 
 for sample_name in mc_sample_names + data_sample_names:
     TopExamples.grid.Add(sample_name).datasets = samples_DXAOD[sample_name]
 
+TopExamples.grid.submit(data_config, TopExamples.grid.Samples(data_sample_names))
 TopExamples.grid.submit(mc_config, TopExamples.grid.Samples(mc_sample_names))
-# TopExamples.grid.submit(data_config, TopExamples.grid.Samples(data_sample_names))
--- grid/submit_gammajet.py
+++ grid/submit_gammajet.py
@@ -2,7 +2,7 @@
 import TopExamples.grid
 from samples import samples_DXAOD
 
-suffix = 'gammajet_20180405_syst_v7'
+suffix = 'gammajet_20180504_syst'
 
 mc_config = TopExamples.grid.Config()
 mc_config.settingsFile  = 'cuts/cuts_gamma_jet_mc15c.txt'
@@ -15,27 +15,26 @@ data_config.suffix         = 'data_'+suffix
 for config in [mc_config, data_config]:
     config.code          = 'top-xaod'
     config.gridUsername  = 'rnewhous'
-    config.excludedSites = ''
-    config.noSubmit      = False
-    # config.noSubmit      = True
+    config.excludedSites = 'ANALY_MPPMU'
+    config.noSubmit      = True
     config.memory        = '1999' # A lot of sites are short on RAM, so we avoid brokerage issues this way
     config.mergeType     = 'Default' #'None', 'Default' or 'xAOD'
     config.destSE        = '' #This is the default (anywhere), or try e.g. 'UKI-SOUTHGRID-BHAM-HEP_LOCALGROUPDISK'
-    config.otherOptions  = '--maxNFilesPerJob=1'
-    # config.otherOptions  = '-- useNewCode --allowTaskDuplication' 
-    
+    # config.otherOptions  = '-- useNewCode --allowTaskDuplication'  
+mc_config.otherOptions  = '--maxNFilesPerJob=1'
+
+
 data_sample_names = [ 'data_2016', 'data_2015' ]
 
 mc_sample_names = [
-    # 'pythia_gammajet',
-    # 'sherpa_gammajet',
-    # 'ttbar_gamma',
-    # 'wz_gamma',
-    'resubmit_gammajet',
+    'pythia_gammajet',
+    'sherpa_gammajet',
+    'ttbar_gamma',
+    'wz_gamma',
     ]
 
 for sample_name in mc_sample_names + data_sample_names:
     TopExamples.grid.Add(sample_name).datasets = samples_DXAOD[sample_name]
 
-# TopExamples.grid.submit(data_config, TopExamples.grid.Samples(data_sample_names))
+TopExamples.grid.submit(data_config, TopExamples.grid.Samples(data_sample_names))
 TopExamples.grid.submit(mc_config, TopExamples.grid.Samples(mc_sample_names))
